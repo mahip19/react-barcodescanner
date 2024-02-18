44788fa4fae51ba7f6280ea770e029c2a3d06911
Mahip <mahipparekh1920@gmail.com>
2024-02-18 20:07:21 +0530

    updated h1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     
     return (
         <div className="App">
-            <h1>Barcode scanner</h1>
+            <h1>Barcode scanner by mahip</h1>
 
             <BarcodeScanner onBarcodeScanned={handleOnBarcodeScanned}/>
 
